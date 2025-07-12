f076082bc099c30721c319a0c43bd07174b57866
Mauri <mauri.monzon91@gmail.com>
2025-07-12 00:30:47 -0300

    Paginacion

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -10,6 +10,7 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { Repository } from 'typeorm';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
 @Injectable()
 export class ProductsService {
@@ -32,9 +33,14 @@ export class ProductsService {
     }
   }
 
-  async findAll() {
+  async findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
     try {
-      const products = await this.productRepository.find();
+      const products = await this.productRepository.find({
+        take: limit,
+        skip: offset,
+        //TODO: relaciones
+      });
       return products;
     } catch (error) {
       this.handleDBExceptions(error);
