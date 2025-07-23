05257ff4b028f50aabdbb1dbd878402003ccf5f9
Mauri <mauri.monzon91@gmail.com>
2025-07-23 11:16:22 -0300

    Seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from '../products/products.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
@@ -12,6 +13,17 @@ export class SeedService {
 
   private async createInitialData() {
     await this.ProductsService.deleteAllProducts();
+
+    const products = initialData.products;
+
+    const inserPromises: Promise<any>[] = [];
+
+    products.forEach((product) => {
+      inserPromises.push(this.ProductsService.create(product));
+    });
+
+    await Promise.all(inserPromises);
+
     return 'Initial data created successfully';
   }
 }
