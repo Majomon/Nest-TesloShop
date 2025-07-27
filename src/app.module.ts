cf49ca3420da559de651e07dfe275d05c54bd6b8
Mauri <mauri.monzon91@gmail.com>
2025-07-26 23:47:24 -0300

    Res files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,29 +3,24 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
-import { SeedModule } from './seed/seed.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      /*      host: process.env.DB_HOST,
+      host: process.env.DB_HOST,
       port: +process.env.DB_PORT!,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD, */
-      host: 'aws-0-us-east-1.pooler.supabase.com',
-      port: 6543,
-      username: 'postgres.lbiuspfypeeagadnbehd',
-      password: 'MIau8COqzpHufm9a',
-      database: 'postgres',
+      password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
     }),
     ProductsModule,
     CommonModule,
-    SeedModule,
+    FilesModule,
   ],
   controllers: [],
   providers: [],
