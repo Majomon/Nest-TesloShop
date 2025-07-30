2064ccfac0466861ba9d9cff6143d4e58fd3161a
Mauri <mauri.monzon91@gmail.com>
2025-07-29 23:23:28 -0300

    Instalado jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => {
+        // console.log('JWT_SECRET:', process.env.JWT_SECRET);
+
+        return {
+          secret: process.env.JWT_SECRET,
+          signOptions: { expiresIn: '2h' },
+        };
+      },
+    }),
+    /*    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '2h' },
+    }), */
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [TypeOrmModule],
