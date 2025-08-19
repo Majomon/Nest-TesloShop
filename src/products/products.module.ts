13b247572cba1e29293de641d61e0c62160479ea
Mauri <mauri.monzon91@gmail.com>
2025-08-18 23:45:14 -0300

    Semilla

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,12 +4,13 @@ import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Product, ProductImage } from './entities';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
   //Defino todas las entidades
-  imports: [TypeOrmModule.forFeature([Product, ProductImage])],
+  imports: [TypeOrmModule.forFeature([Product, ProductImage]), AuthModule],
   exports: [ProductsService, TypeOrmModule],
 })
 export class ProductsModule {}
