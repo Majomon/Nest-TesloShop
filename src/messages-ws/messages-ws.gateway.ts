db0a09ad02e7cfa79fcfb1db500d01e4dbe014ea
Mauri <mauri.monzon91@gmail.com>
2025-10-31 23:13:22 -0300

    conexcion

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -12,14 +12,11 @@ export class MessagesWsGateway
 {
   constructor(private readonly messagesWsService: MessagesWsService) {}
   handleConnection(client: Socket) {
-    // console.log('Cliente conectado: ', client.id);
     this.messagesWsService.registerClient(client);
-
-    console.log({ conectados: this.messagesWsService.getConnectedClients() });
+    // console.log({ conectados: this.messagesWsService.getConnectedClients() });
   }
   handleDisconnect(client: Socket) {
-    // console.log('Cliente desconectado: ', client.id);
     this.messagesWsService.removeClient(client.id);
-    console.log({ conectados: this.messagesWsService.getConnectedClients() });
+    // console.log({ conectados: this.messagesWsService.getConnectedClients() });
   }
 }
